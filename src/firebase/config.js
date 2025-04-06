be29da2cbb7750dc5cd181ba1ee15851bc3aff76
Orçun Koca <75696946+kocaOrcun@users.noreply.github.com>
2025-04-06 17:15:26 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,14 +6,7 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyA9-mhdtDExETYKerSzgu8GnQch5E2qdUQ",
-    authDomain: "mealicious-71223.firebaseapp.com",
-    projectId: "mealicious-71223",
-    storageBucket: "mealicious-71223.appspot.com",
-    messagingSenderId: "530293984548",
-    appId: "1:530293984548:web:e390905558be8c3b3b37b1"
-};
+
 
 // init firebase
 firebase.initializeApp(firebaseConfig)
